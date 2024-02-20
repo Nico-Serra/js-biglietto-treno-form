4933889c1806beb98e37d473fee488bce19a5ce3
Nico-Serra <nico.serra000@gmail.com>
2024-02-20 15:51:43 +0100

    fix milestone 1

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,16 @@
 console.log('Funziona');
 
 const priceKm = 0.21
-console.log(typeof priceKm);
+//console.log(typeof priceKm);
+const discountMinor = 0.2
+const discountSenior = 0.4
 
 //- Chiedere all'utente il numero di km da percorrere e la propria età
-const trip = document.getElementById('km');
+let trip = document.getElementById('km');
 
 //console.log(typeof trip);
 
-const userAge = document.getElementById('age');
+let userAge = document.getElementById('age');
 
 //console.log(userAge);
 
@@ -16,13 +18,31 @@ document.querySelector('button').addEventListener('click', function () {
     //alert('funziona')
     console.log(trip.value);
     console.log(userAge.value);
+    trip = (trip.value)
+    userAge = (userAge.value)
+    //- Stabilire il prezzo totale del biglietto;
+
+    let priceTicket = trip * priceKm
+    console.log(priceTicket);
+   
+    if (userAge < 18) {
+        //-SE il passeggero è minorenne viene applicato uno sconto del 20%
+        priceTicket = priceTicket - (priceTicket * discountMinor)
+        console.log(priceTicket);
+    } else if (userAge > 65) {
+        //- ALTRIMENTI SE il passeggero è over65 viene applicato uno sconto del 40%
+        priceTicket = priceTicket - (priceTicket * discountSenior)
+        console.log(priceTicket);
+    }
 });
 
 
-//- Stabilire il prezzo totale del biglietto
-    //let priceTicket = trip.value * priceKm
-   // console.log(priceTicket);
 
-    //-SE il passeggero è minorenne viene applicato uno sconto del 20%
-    //- ALTRIMENTI SE il passeggero è over65 viene applicato uno sconto del 40%
+
+
+
+
+
+    
+    
 //- Stampare il prezzo del biglietto in forma umana
--- readme.md
+++ readme.md
@@ -21,7 +21,7 @@ MILESTONE 2:
 Solo una volta che il milestone 1 sarà completo e funzionante allora realizzeremo un form in pagina in cui l’utente potrà inserire i dati e visualizzare il calcolo finale con il prezzo. Il recap dei dati e l'output del prezzo finale, andranno quindi stampati in pagina (il prezzo dovrà essere formattato con massimo due decimali, per indicare i centesimi sul prezzo). Questo richiederà un minimo di ricerca.
 
 # Steps
-- Chiedere all'utente il numero di km da percorrere e la propria età
+- Chiedere all'utente il numero di km da percorrere e la propria età attraverso un imput
 - Stabilire il prezzo totale del biglietto
     -SE il passeggero è minorenne viene applicato uno sconto del 20%
     - ALTRIMENTI SE il passeggero è over65 viene applicato uno sconto del 40%
